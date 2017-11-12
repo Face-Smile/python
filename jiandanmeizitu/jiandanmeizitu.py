9710c090ab3fb9c4d891958819e05dea95c3b036
Face-Smile <22629374+Face-Smile@users.noreply.github.com>
2017-11-12 16:47:29 +0800

    set headers，or it will be 404

--- jiandanmeizitu/jiandanmeizitu.py
+++ jiandanmeizitu/jiandanmeizitu.py
@@ -1,5 +1,5 @@
-
 # coding:gbk
+import requests
 import urllib2
 import re
 import threading
@@ -9,14 +9,24 @@ def download(i):
     global num
     url='http://jandan.net/ooxx/page-%d#comments' %i
     urls=[]
-    req=urllib2.urlopen(url).read()
+    print url
+    headers={"Host":"jandan.net",
+"User-Agent":"Mozilla/5.0 (Windows NT 6.1; W…) Gecko/20100101 Firefox/56.0",
+"Accept":"text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8",
+"Accept-Language":"zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",	
+"Accept-Encoding":"gzip, deflate",
+"Cookie	":"__cfduid=d3cc76cfacc58f3f53dedaf1426fc68d11510458274",
+"Connection":"keep-alive"}
+    req=requests.get(url, headers=headers)
+    req=req.text
     txt=re.compile(r'</a><br /><img src="//(.*?\.jpg)')
     result=txt.findall(req)
     for ii in result:
         urls.append(ii)
     for i in urls:
         t='http://'+i
-        pic=urllib2.urlopen(t, timeout=10)
+        print t
+        pic=requests.get(t, timeout=10)
         p=re.findall(r'[A-z0-9]+\.jpg',i)
         p=''.join(p)
         isExists=os.path.exists(path)
@@ -24,19 +34,21 @@ def download(i):
             os.makedirs(path)
         string='%s/%s' %(path,p)
         with open(string,'wb') as f:
-            f.write(pic.read())
-            print '%s下载完成\n' %p,
+            f.write(pic.content)
+            print p+u'下载完成'
             num+=1
 
 start=input('请输入起始页：')
 end=input('请输入终止页：')
-path=raw_input('请输入下载地址:')
+path=raw_input('请输入下载保存地址:')
 print '开始下载'
 if __name__=='__main__':
+    num=0
+    threads=[]
     for i in range(start,end+1):
         t=threading.Thread(target=download,args=(i,))
         t.start()
         threads.append(t)
         t.join()
 print '一共下载了%d张图片' %num
-raw_input("please input ENTER to exit")
+
